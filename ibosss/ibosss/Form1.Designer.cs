6ac83448d31d7c5f579389a60320fa7fcf34fa77
ademyalcindag <ademyalcindag2@gmail.com>
2022-05-18 09:28:33 +0300

    c# ile sinema otomasyon
    sinema bileti almak için film seç koltuk
    seç vb. gibi bir arayüze sahip ekran

---
+++ ibosss/ibosss/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace ibosss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sinemaDataSet = new ibosss.sinemaDataSet();
+            this.table1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.table_1TableAdapter = new ibosss.sinemaDataSetTableAdapters.Table_1TableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.table1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.table2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.table_2TableAdapter = new ibosss.sinemaDataSetTableAdapters.Table_2TableAdapter();
+            this.sinemaDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.filmıdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmyntmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmypmyılıDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.sinemaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sinemaDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 32);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filmi seçiniz ?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "seans seçiniz ?";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(824, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "müşteri seçiniz ?";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(288, 32);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(533, 7);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "kontuk seçiniz ?";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(508, 32);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(204, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(776, 32);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(204, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1093, 7);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Filmi seçiniz ?";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sinemaDataSet
+            // 
+            this.sinemaDataSet.DataSetName = "sinemaDataSet";
+            this.sinemaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table1BindingSource
+            // 
+            this.table1BindingSource.DataMember = "Table_1";
+            this.table1BindingSource.DataSource = this.sinemaDataSet;
+            // 
+            // table_1TableAdapter
+            // 
+            this.table_1TableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.filmıdDataGridViewTextBoxColumn,
+            this.filmadDataGridViewTextBoxColumn,
+            this.filmyntmDataGridViewTextBoxColumn,
+            this.filmypmyılıDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.table1BindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 167);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(925, 174);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "seç";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(827, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "seç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(536, 84);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "seç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // table1BindingSource1
+            // 
+            this.table1BindingSource1.DataMember = "Table_1";
+            this.table1BindingSource1.DataSource = this.sinemaDataSet;
+            // 
+            // table2BindingSource
+            // 
+            this.table2BindingSource.DataMember = "Table_2";
+            this.table2BindingSource.DataSource = this.sinemaDataSet;
+            // 
+            // table_2TableAdapter
+            // 
+            this.table_2TableAdapter.ClearBeforeFill = true;
+            // 
+            // sinemaDataSetBindingSource
+            // 
+            this.sinemaDataSetBindingSource.DataSource = this.sinemaDataSet;
+            this.sinemaDataSetBindingSource.Position = 0;
+            // 
+            // filmıdDataGridViewTextBoxColumn
+            // 
+            this.filmıdDataGridViewTextBoxColumn.DataPropertyName = "filmıd";
+            this.filmıdDataGridViewTextBoxColumn.HeaderText = "filmıd";
+            this.filmıdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.filmıdDataGridViewTextBoxColumn.Name = "filmıdDataGridViewTextBoxColumn";
+            this.filmıdDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // filmadDataGridViewTextBoxColumn
+            // 
+            this.filmadDataGridViewTextBoxColumn.DataPropertyName = "filmad";
+            this.filmadDataGridViewTextBoxColumn.HeaderText = "filmad";
+            this.filmadDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.filmadDataGridViewTextBoxColumn.Name = "filmadDataGridViewTextBoxColumn";
+            this.filmadDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // filmyntmDataGridViewTextBoxColumn
+            // 
+            this.filmyntmDataGridViewTextBoxColumn.DataPropertyName = "filmyntm";
+            this.filmyntmDataGridViewTextBoxColumn.HeaderText = "filmyntm";
+            this.filmyntmDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.filmyntmDataGridViewTextBoxColumn.Name = "filmyntmDataGridViewTextBoxColumn";
+            this.filmyntmDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // filmypmyılıDataGridViewTextBoxColumn
+            // 
+            this.filmypmyılıDataGridViewTextBoxColumn.DataPropertyName = "filmypmyılı";
+            this.filmypmyılıDataGridViewTextBoxColumn.HeaderText = "filmypmyılı";
+            this.filmypmyılıDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.filmypmyılıDataGridViewTextBoxColumn.Name = "filmypmyılıDataGridViewTextBoxColumn";
+            this.filmypmyılıDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1036, 410);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "sinema otomasyon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sinemaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sinemaDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private sinemaDataSet sinemaDataSet;
+        private System.Windows.Forms.BindingSource table1BindingSource;
+        private sinemaDataSetTableAdapters.Table_1TableAdapter table_1TableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.BindingSource table1BindingSource1;
+        private System.Windows.Forms.BindingSource table2BindingSource;
+        private sinemaDataSetTableAdapters.Table_2TableAdapter table_2TableAdapter;
+        private System.Windows.Forms.BindingSource sinemaDataSetBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmıdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmyntmDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmypmyılıDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ ibosss/ibosss/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;  
+
+
+namespace ibosss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglantı = new SqlConnection("Data Source=(LocalDb)MSSQLLocalDB;Initial Catalog=sinema;Integrated Security=True");
+        SqlCommand komut;
+        SqlDataAdapter adtr ;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sinemaDataSet.Table_2' table. You can move, or remove it, as needed.
+            this.table_2TableAdapter.Fill(this.sinemaDataSet.Table_2);
+            // TODO: This line of code loads data into the 'sinemaDataSet.Table_1' table. You can move, or remove it, as needed.
+            this.table_1TableAdapter.Fill(this.sinemaDataSet.Table_1);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglantı.Open();
+            komut = new SqlCommand("select *from filmad", baglantı);
+            adtr = new SqlDataAdapter(komut);
+            baglantı.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
